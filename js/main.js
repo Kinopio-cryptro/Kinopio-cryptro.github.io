ce4af4cf04b42769fe298dbb88af622aad09221d
Bús Rudolf <busrudi@gmail.com>
2021-07-17 15:34:58 +0200

    Add new html+css+js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+huf = document.getElementById("huf").value;
+dev = document.getElementById("deviza").value;
+
+document.getElementById("calc").addEventListener("click", () => {
+    document.getElementById("result").innerHTML = "Friss dinnye";
+})
