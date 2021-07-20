b10b0481f271ee1a43a4e0f100e6e388e484c93d
Bús Rudolf <busrudi@gmail.com>
2021-07-20 20:48:55 +0200

    Modified .js with the calculation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-huf = document.getElementById("huf").value;
-dev = document.getElementById("deviza").value;
+const huf = document.getElementById("huf").value;
+const dev = document.getElementById("deviza").value;
 
 document.getElementById("calc").addEventListener("click", () => {
-    document.getElementById("result").innerHTML = "Friss dinnye";
+    document.getElementById("result").innerHTML = ((textForm(huf) / textForm(dev)).toFixed(2)) * 100
 })
+
+function textForm(param) {
+    const arr = param.split("");
+
+    while (arr.indexOf(" ") > 0) {
+        arr.splice(arr.indexOf(" "), 1);
+    }
+
+    param = arr.join('');
+    console.log(typeof (arr));
+    return parseFloat(param);
+}
