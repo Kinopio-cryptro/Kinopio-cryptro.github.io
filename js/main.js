73fce99670e47416b7cd0b7a984e77c5fbc5e1b6
Bús Rudolf <busrudi@gmail.com>
2021-07-24 15:33:11 +0200

    modified all

--- js/main.js
+++ js/main.js
@@ -2,17 +2,16 @@ const huf = document.getElementById("huf").value;
 const dev = document.getElementById("deviza").value;
 
 document.getElementById("calc").addEventListener("click", () => {
-    document.getElementById("result").innerHTML = ((textForm(huf) / textForm(dev)).toFixed(2)) * 100
+    document.getElementById("result").innerHTML = (textForm(huf) / textForm(dev)).toFixed(2) * 100
 })
 
 function textForm(param) {
+    param = param.replace(",", ".")
     const arr = param.split("");
 
     while (arr.indexOf(" ") > 0) {
         arr.splice(arr.indexOf(" "), 1);
     }
 
-    param = arr.join('');
-    console.log(typeof (arr));
-    return parseFloat(param);
+    return parseFloat(arr.join(''));
 }
